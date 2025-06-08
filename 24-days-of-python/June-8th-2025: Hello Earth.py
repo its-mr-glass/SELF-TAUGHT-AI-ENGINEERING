d424f1b84ba96f6f32d1540c0bd53f8da1d25628
The Man <167177617+LamarJonesNdubi@users.noreply.github.com>
2025-06-08 12:54:06 +0300

    Add files via upload

---
+++ 24-days-of-python/June-8th-2025: Hello Earth.py
@@ -0,0 +1,9 @@
+print("June-8th-2025: HELLO EARTH by Lamar")
+print("Hello there :)")
+
+name = input("Please input your name: ")
+age = int(input("Please enter your birth year: "))
+current_year = int(input("Enter the current year: "))
+
+print(f"Hello there {name} you are {current_year - age} years old :)")
+
