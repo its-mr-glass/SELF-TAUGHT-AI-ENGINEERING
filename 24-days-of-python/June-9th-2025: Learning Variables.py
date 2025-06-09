10d02550ff602e63653f267427f860bdcb0227e4
LamarJonesNdubi <lamarjonesndubi@gmail.com>
2025-06-09 16:19:20 +0300

    variebles

---
+++ 24-days-of-python/June-9th-2025: Learning Variables.py
@@ -0,0 +1,46 @@
+print("June-9th-2025: Learning Variables")
+# Variable = A container for a value (string, integer, float, boolean).
+#            A variable behaves as if it is the value it contains.
+
+# strings
+# Contains mostly words with quotations even when numbers included
+first_name = "Lamar"
+food = "Pizza"
+email = "Lamar123@fake.com"
+
+print(first_name)
+
+
+# You can also use the <f-string> its the easiest way to print a format.
+
+print(f"Hello {first_name}")
+print(f"You love {food}")
+print(f"Your email address is {email}.")
+
+# integers
+# Contains mostly numbers
+age = 23
+quantity = 7
+acres = 100
+
+print(f"You are {age} years old")
+print(f"Have bought {quantity} of cement")
+print(f"Your land {acres} sold")
+
+# Floats
+# Contain decimales or points
+age = 23.5
+quantity = 7.5
+acres = 100.5
+
+print(f"You are {age} years old")
+print(f"Have bought {quantity} of cement")
+print(f"Your land {acres} sold")
+
+# Boolean
+# Contains confermations: True or False
+is_student = False
+is_self_taught = True 
+
+print(f"Are you a student?: {is_student}")
+print(f"Are you are on a self taught AI Journey?: {is_self_taught}")
