d0629401b24ae86c774319efeb050a29649cd151
LamarJonesNdubi <lamarjonesndubi@gmail.com>
2025-06-10 08:51:53 +0300

    user input

---
+++ June-10th-2025: User  Input + Exercises.py
@@ -0,0 +1,9 @@
+print("June-10th-2025: User Input + Exercises")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+current_year = int(input("Enter your current year: "))
+
+print(f"Hello {name}")
+print(f"You are {age} years old")
+
