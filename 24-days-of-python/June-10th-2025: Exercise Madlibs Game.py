d6dc7c63caee2671752b3f61fab44a19cb58dd09
LamarJonesNdubi <lamarjonesndubi@gmail.com>
2025-06-10 09:09:05 +0300

    Madlibs exercise game

---
+++ 24-days-of-python/June-10th-2025: Exercise Madlibs Game.py
@@ -0,0 +1,12 @@
+print("June-10th-2025: Exercise Madlibs Game")
+
+adjective1 = input("Enter an adjective: ") # an adjective explains the quality of a thing eg.(fast, slow, suspeciaus)
+noun = input("Enter a noun: ")# a noun is a name, place or a thing eg(mark zuckerburg, cow, Diddy)
+adjective2 = input("Enter an adjective: ")
+verb = input("Enter a verb: ")# a verb is an action eg.(Run, scritching, laughting, fighting)
+adjective3 = input("Enter an adjective: ")
+
+print(f"Today i went to a {adjective1} zoo")
+print(f"In a exhibit, i saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
